018a9926583be0fa8292ba2c5b24bb8e8bd09b05
Victor Yamchinov <kodiers@gmail.com>
2020-03-27 01:08:31 +0300

    added layout for mainvc

--- RNDM/Controller/MainVC.swift
+++ RNDM/Controller/MainVC.swift
@@ -15,13 +15,33 @@ enum ThoughtCategory: String {
     case popular = "popular"
 }
 
-class MainVC: UIViewController {
+class MainVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet private weak var segmentControl: UISegmentedControl!
+    @IBOutlet private weak var tableView: UITableView!
+    
+    private var thoughts = [Thought]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.estimatedRowHeight = 80
+        tableView.rowHeight = UITableView.automaticDimension
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return thoughts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "thoughtCell", for: indexPath) as? ThoughtCell {
+            cell.configureCell(thought: thoughts[indexPath.row])
+            return cell
+        }
+        return UITableViewCell()
+    }
 
 }
 
---
+++ RNDM/View/ThoughtCell.swift
@@ -0,0 +1,30 @@
+//
+//  ThoughtCell.swift
+//  RNDM
+//
+//  Created by Viktor Yamchinov on 27.03.2020.
+//  Copyright © 2020 Viktor Yamchinov. All rights reserved.
+//
+
+import UIKit
+
+class ThoughtCell: UITableViewCell {
+    
+    @IBOutlet private weak var usernameLbl: UILabel!
+    @IBOutlet private weak var timestampLbl: UILabel!
+    @IBOutlet private weak var thoughtTxtLbl: UILabel!
+    @IBOutlet private weak var likesImg: UIImageView!
+    @IBOutlet private weak var likesNumLbl: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    func configureCell(thought: Thought) {
+        self.usernameLbl.text = thought.username
+        self.thoughtTxtLbl.text = thought.thoughtTxt
+    }
+
+}
